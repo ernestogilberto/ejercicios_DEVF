569ded0ce20c8d833160dbd1f5921c4b5c78756d
Gilberto Valenzuela <ernestogilberto@hotmail.com>
2021-08-19 10:57:04 -0500

    Agregando primeros ejercicios de objetos.

---
+++ objects1.js
@@ -0,0 +1,12 @@
+/*1. Write a JavaScript program to list the properties of a JavaScript object.
+    Sample object:*/
+
+var student = {
+    name: "David Rayy",
+    sclass: "VI",
+    rollno: 12
+};
+
+console.log(student.name, student.sclass, student.rollno)
+
+// Sample Output: name,sclass,rollno
